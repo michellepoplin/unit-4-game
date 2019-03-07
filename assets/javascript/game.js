ba6baf783afb94e96a788e851e808fa6cd723fd0
Michelle Poplin <mlip6547@gmail.com>
2019-03-06 22:37:58 -0500

    Revised game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,13 @@
 
 $(document).ready(function(){
-    var yourMatchingNumber = 0;
+    var currentScore = 0;
+    var targetScore = 0;
     var randomNum = randomNumGen();
-    var wins = 0;
-    var losses = 0;
+    var winCount = 0;
+    var lossCount = 0;
     var crystals;
-})
+});
 
-var game = { 
-    crystalValues: [],
-    targetNumber: 0,
-    score: 0,
-    
-    gameSet: function() {
-        this.score = 0;
-        
         for (var i = 0; i < 4; i++) {
             var number = Math.floor(Math.random()* 12) + 1,
             
@@ -30,35 +23,33 @@ var game = {
         $("#numberStart").text(this.targetNumber);
         $("#winner").text(this.wins);
         $("#loser").text(this.losses);
-    },
     
-    guessDisplay: function(a) {
-        game.score += a;
-        $("#totalScore").text(game.score);
+    
+        $("#your-score").text(currentScore);
+        $("#target-score").text(targetScore);
         
-        game.rules();
-    },
+        var checkWin = function(){
     
-    rules: function() {
+    
         if (this.score === this.targetNumber) {
             this.wins++;
             this.gameSet();
-            alert("Congrats You Won!")
-        } else if (this.score > this.targetNumber) {
+            alert("Congrats You Won!");
+            console.log("Your Score: " + currentScore);
+        } 
+        else if (this.score > this.targetNumber) {
             this.losses++;
             this.gameSet();
-            alert("Sorry you Lose!")
+            alert("Sorry you Lose!");
+            console.log("Your Score: " + currentScore);
         }
     }
     
-};
-    
-
-
-
 $("document").ready( function() {
     
-    game.gameSet();
+   var startGame = function(){
+
+    currentScore = 0;
     
     $("#crystalOne").click(function() {
         game.guessDisplay(game.crystalValues[0]);
@@ -77,7 +68,7 @@ $("document").ready( function() {
     });
     
     
-});
+}
 
 
 
@@ -105,81 +96,8 @@ $("document").ready( function() {
 
 
 
-
-
-
-// var game = { 
-//     crystalValue: [],
-//     wins: 0,
-//     loses: 0,
-//     score: 0,
-//     numberGuess: 0,
-//     targetNumber: 0,
-    
-//     setUp: function() {
-//         this.score = 0;
-        
-//         for (var i = 0; i < 4; i++) {
-//             var number = Math.floor(Math.random()*(13 - 1) + 1);
-            
-//             if (this.crystalValues.includes(number)) {
-//                 i--;
-//             } else {
-//                 this.crystalValues[i] = number;
-//             }
-//         }
-        
-//         this.target = Math.floor(Math.random()*(121-19) + 19);
-//         $("#totalScore").text(this.score);
-//         $("#numberToGuess").text(this.targetNumber);
-//         $("#winer").text(this.wins);
-//         $("#losser").text(this.loses);
-//     },
-    
-//     display: function(a) {
-//         game.score += a;
-//         $("#totalScore").text(game.score);
-        
-//         game.compare();
-//     },
-    
-//     compare: function() {
-//         if (this.score === this.targetNumber) {
-//             this.wins++;
-//             this.gameSet();
-//         } else if (this.score > this.targetNumber) {
-//             this.loses++;
-//             this.gameSet();
-//         }
-//     }
-    
-// };
-    
-
-
-
-// $("document").ready( function() {
-    
-//     game.gameSet();
-    
-//     $("#crystalOne").click(function() {
-//         game.dispalay(game.crystalValues[0]);
-//     });
-    
-//     $("#crystalTwo").click(function() {
-//         game.display(game.crystalValues[1]);
-//     });
-    
-//     $("#crystalThree").click(function() {
-//         game.display(game.crystalValues[2]);
-//     });
-    
-//     $("#crystalFour").click(function() {
-//         game.display(game.crystalValues[3]);
-//     });
-    
-    
-// });
+  
+ 
 
 
 
@@ -187,52 +105,7 @@ $("document").ready( function() {
 
 
 
-// var game= {
-//     crystalVale: [],
-//     wins: 0,
-//     loses: 0,
-//     score: 0,
-//     numberGuess: 0,
-//     targetNumber: 0,
-    
-        // gameSet: function() {
-        //     this.score = 0;
 
-        //     for (var i = 0; i < 4; i++) {
-        //     var number= Math.floor(math.random()*(13 - 1) + 1);
-        //     if (this.crystalValue.includes(number)) {
-        //          i--;
-        //     } else {
-        //          this.crystalValue[i] = number;
-        //     }
-        // }
-
-    //     this.targetNumber = Math.floor (Math.random()*(121-19)+ 19);
-    //     $("#totalScore").text(this.score);
-    //     $("#numberToGuess").text(this.targetNumber);
-    //     $("winner").text(this.wins);
-    //     $("loser").text(this.loses);
-
-
-    // },
-
-    // Display: function (a) {
-    //     game.score += a;
-    //     $("totalScore").text(game.score);
-
-    //     game.compare();
-    // },
-
-//     compare: fuction() {
-//         if (this.score === this.targetNumber) {
-//             this.wins++;
-//             this.gameSet();
-//         } else if (this.score > this.targetNumber) {
-//             this.loses++;
-//             this.gameSet();
-//         }
-//     }
-// };
 
 
 
